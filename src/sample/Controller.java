263c0e856d72a5cbeb194dabf56945e98721a3ac
Taccco <uiangam@yahoo.com>
2021-05-08 17:28:33 +0800

    start

---
+++ src/sample/Controller.java
@@ -0,0 +1,37 @@
+package sample;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class Controller{
+
+    @FXML
+    private TextField searchField;
+
+    @FXML
+    private Button HomeBut;
+
+    @FXML
+    private Button RecentBut;
+
+    @FXML
+    private Button OptionBut;
+
+    @FXML
+    private Button HelpBut;
+
+    @FXML
+    private Button ExitBut;
+
+    @FXML
+    void HomeBut(ActionEvent event) {
+
+    }
+
+    @FXML
+    void RecentBut(ActionEvent event) {
+
+    }
+
+}
