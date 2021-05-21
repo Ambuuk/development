2d7caa61e13c6e07b07094424fc4eae93cb64dcc
ambuuk <ambuuk@gmail.com>
2021-05-21 23:00:25 +0800

    push

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -13,165 +13,130 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class Controller {
-
     @FXML
     private TextField searchField;
-
     @FXML
     private Button HomeBut;
-
     @FXML
     private Button HelpBut;
-
     @FXML
     private Button ExitBut;
-
     @FXML
     private FlowPane flow;
-
     @FXML
     private Button officeBut;
-
     @FXML
     private Button JavaBut;
-
     @FXML
     private Button MavenBut;
-
     @FXML
     private Button EclipseBut;
-
     @FXML
     private Button devCBut;
-
     @FXML
     private Button codeBlocksBut;
-
     @FXML
     private Button intelliJBut;
-
     @FXML
     private Button teamsBut;
-
     @FXML
     private Button andrStudBut;
-
-    @FXML
-    void HomeBut(ActionEvent event) {
-
-    }
+    /*Programiig haah button*/
     @FXML
     void exitButton(ActionEvent event) {
         Stage stage = (Stage) ExitBut.getScene().getWindow();
         stage.close();
     }
-
     @FXML
     void helpButton(ActionEvent event) {
-
     }
-
     @FXML
     void homeButton(ActionEvent event) {
-
     }
+    /* Android studio tsonh neegdej AndroidStudio.fxml viewiig awna
+    * Uunees dooshih buh view uud javaController controllertoi*/
     @FXML
     void openAndroidStudio(ActionEvent event) throws IOException {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("AndroidStudio.fxml"));
         Parent root = loader.load();
-
         Stage stage = new Stage();
         stage.setScene(new Scene(root));
         stage.setTitle("Android Studio");
         stage.show();
     }
-
+    /* CodeBlocks studio tsonh neegdej CodeBlocks.fxml viewiig awna */
     @FXML
     void openCodeBlocks(ActionEvent event) throws IOException {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("CodeBlocks.fxml"));
         Parent root = loader.load();
-
         Stage stage = new Stage();
         stage.setScene(new Scene(root));
         stage.setTitle("Code Blocks");
         stage.show();
     }
-
+    /* DevC tsonh neegdej DevC.fxml viewiig awna */
     @FXML
     void openDevC(ActionEvent event) throws IOException {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("DevC.fxml"));
         Parent root = loader.load();
-
         Stage stage = new Stage();
         stage.setScene(new Scene(root));
         stage.setTitle("DevC++");
         stage.show();
     }
-
+    /* Eclipse tsonh neegdej Eclipse.fxml viewiig awna */
     @FXML
     void openEclipse(ActionEvent event) throws IOException {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("Eclipse.fxml"));
         Parent root = loader.load();
-
         Stage stage = new Stage();
         stage.setScene(new Scene(root));
         stage.setTitle("Eclipse");
         stage.show();
     }
-
     @FXML
     void openIntelliJ(ActionEvent event) throws IOException {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("IntelliJ.fxml"));
         Parent root = loader.load();
-
         Stage stage = new Stage();
         stage.setScene(new Scene(root));
         stage.setTitle("IntelliJ");
         stage.show();
     }
-
     @FXML
     void openJava(ActionEvent event) throws IOException {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("Java.fxml"));
         Parent root = loader.load();
-
         Stage stage = new Stage();
         stage.setScene(new Scene(root));
         stage.setTitle("Java");
         stage.show();
     }
-
     @FXML
     void openMaven(ActionEvent event) throws IOException {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("Maven.fxml"));
         Parent root = loader.load();
-
         Stage stage = new Stage();
         stage.setScene(new Scene(root));
         stage.setTitle("Maven");
         stage.show();
     }
-
     @FXML
     void openOffice(ActionEvent event) throws IOException {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("Office.fxml"));
         Parent root = loader.load();
-
         Stage stage = new Stage();
         stage.setScene(new Scene(root));
         stage.setTitle("Office");
         stage.show();
     }
-
     @FXML
     void openTeams(ActionEvent event) throws IOException {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("Teams.fxml"));
         Parent root = loader.load();
-
         Stage stage = new Stage();
         stage.setScene(new Scene(root));
         stage.setTitle("Teams");
         stage.show();
     }
-
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -7,16 +7,15 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application {
-
     @Override
     public void start(Stage primaryStage) throws Exception{
+        /* Main activity
+        * sample.fxml main view ee awj ajilluulna*/
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("AUSoft");
         primaryStage.setScene(new Scene(root, 700, 500));
         primaryStage.show();
     }
-
-
     public static void main(String[] args) {
         launch(args);
     }
